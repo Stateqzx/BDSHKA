de09697193538cff01b42fb2a0d445447e5d77b2
Stateqzx <tan.rayder@yandex.ru>
2022-11-04 01:18:38 +0700

    Добавьте файлы проекта.

---
+++ BDSHKA/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace BDSHKA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.____ShDanDataSet = new BDSHKA.____ShDanDataSet();
+            this.туристыBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.туристы_TableAdapter = new BDSHKA.____ShDanDataSetTableAdapters.Туристы_TableAdapter();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.фамилияDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.имяDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.отчествоDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.паспортDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.телефонDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.еmейлDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.регистрацияDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.страховойПолисDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.полDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.загранпаспортDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.____ShDanDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.туристыBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.фамилияDataGridViewTextBoxColumn,
+            this.имяDataGridViewTextBoxColumn,
+            this.отчествоDataGridViewTextBoxColumn,
+            this.паспортDataGridViewTextBoxColumn,
+            this.телефонDataGridViewTextBoxColumn,
+            this.еmейлDataGridViewTextBoxColumn,
+            this.регистрацияDataGridViewTextBoxColumn,
+            this.страховойПолисDataGridViewTextBoxColumn,
+            this.полDataGridViewTextBoxColumn,
+            this.загранпаспортDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.туристыBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(71, 155);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(614, 267);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ____ShDanDataSet
+            // 
+            this.____ShDanDataSet.DataSetName = "____ShDanDataSet";
+            this.____ShDanDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // туристыBindingSource
+            // 
+            this.туристыBindingSource.DataMember = "Туристы$";
+            this.туристыBindingSource.DataSource = this.____ShDanDataSet;
+            // 
+            // туристы_TableAdapter
+            // 
+            this.туристы_TableAdapter.ClearBeforeFill = true;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // фамилияDataGridViewTextBoxColumn
+            // 
+            this.фамилияDataGridViewTextBoxColumn.DataPropertyName = "Фамилия";
+            this.фамилияDataGridViewTextBoxColumn.HeaderText = "Фамилия";
+            this.фамилияDataGridViewTextBoxColumn.Name = "фамилияDataGridViewTextBoxColumn";
+            // 
+            // имяDataGridViewTextBoxColumn
+            // 
+            this.имяDataGridViewTextBoxColumn.DataPropertyName = "Имя";
+            this.имяDataGridViewTextBoxColumn.HeaderText = "Имя";
+            this.имяDataGridViewTextBoxColumn.Name = "имяDataGridViewTextBoxColumn";
+            // 
+            // отчествоDataGridViewTextBoxColumn
+            // 
+            this.отчествоDataGridViewTextBoxColumn.DataPropertyName = "Отчество";
+            this.отчествоDataGridViewTextBoxColumn.HeaderText = "Отчество";
+            this.отчествоDataGridViewTextBoxColumn.Name = "отчествоDataGridViewTextBoxColumn";
+            // 
+            // паспортDataGridViewTextBoxColumn
+            // 
+            this.паспортDataGridViewTextBoxColumn.DataPropertyName = "паспорт";
+            this.паспортDataGridViewTextBoxColumn.HeaderText = "паспорт";
+            this.паспортDataGridViewTextBoxColumn.Name = "паспортDataGridViewTextBoxColumn";
+            // 
+            // телефонDataGridViewTextBoxColumn
+            // 
+            this.телефонDataGridViewTextBoxColumn.DataPropertyName = "телефон";
+            this.телефонDataGridViewTextBoxColumn.HeaderText = "телефон";
+            this.телефонDataGridViewTextBoxColumn.Name = "телефонDataGridViewTextBoxColumn";
+            // 
+            // еmейлDataGridViewTextBoxColumn
+            // 
+            this.еmейлDataGridViewTextBoxColumn.DataPropertyName = "еmейл";
+            this.еmейлDataGridViewTextBoxColumn.HeaderText = "еmейл";
+            this.еmейлDataGridViewTextBoxColumn.Name = "еmейлDataGridViewTextBoxColumn";
+            // 
+            // регистрацияDataGridViewTextBoxColumn
+            // 
+            this.регистрацияDataGridViewTextBoxColumn.DataPropertyName = "регистрация";
+            this.регистрацияDataGridViewTextBoxColumn.HeaderText = "регистрация";
+            this.регистрацияDataGridViewTextBoxColumn.Name = "регистрацияDataGridViewTextBoxColumn";
+            // 
+            // страховойПолисDataGridViewTextBoxColumn
+            // 
+            this.страховойПолисDataGridViewTextBoxColumn.DataPropertyName = "страховой полис";
+            this.страховойПолисDataGridViewTextBoxColumn.HeaderText = "страховой полис";
+            this.страховойПолисDataGridViewTextBoxColumn.Name = "страховойПолисDataGridViewTextBoxColumn";
+            // 
+            // полDataGridViewTextBoxColumn
+            // 
+            this.полDataGridViewTextBoxColumn.DataPropertyName = "пол";
+            this.полDataGridViewTextBoxColumn.HeaderText = "пол";
+            this.полDataGridViewTextBoxColumn.Name = "полDataGridViewTextBoxColumn";
+            // 
+            // загранпаспортDataGridViewTextBoxColumn
+            // 
+            this.загранпаспортDataGridViewTextBoxColumn.DataPropertyName = "загранпаспорт";
+            this.загранпаспортDataGridViewTextBoxColumn.HeaderText = "загранпаспорт";
+            this.загранпаспортDataGridViewTextBoxColumn.Name = "загранпаспортDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.____ShDanDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.туристыBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private ____ShDanDataSet ____ShDanDataSet;
+        private System.Windows.Forms.BindingSource туристыBindingSource;
+        private ____ShDanDataSetTableAdapters.Туристы_TableAdapter туристы_TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn фамилияDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn имяDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn отчествоDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn паспортDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn телефонDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn еmейлDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn регистрацияDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn страховойПолисDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn полDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn загранпаспортDataGridViewTextBoxColumn;
+    }
+}
+
